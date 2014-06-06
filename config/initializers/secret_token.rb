dd3f2693aba148b711ece2793faa247321d93cfd
Prerna Jaiswal <prerna11081@iiitd.ac.in>
2014-06-06 13:00:58 +0530

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Edu::Application.config.secret_key_base = 'a3b05b5396a8bf5a069a647d4556319e13ce63bc74236c88ef2c0599a746f6490c86cd1c4332d0776055a198b58f310b692160a786ef88ef7306c0dcc785318a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Edu::Application.config.session_store :cookie_store, key: '_Edu_session'
