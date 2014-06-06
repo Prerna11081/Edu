3939050a1fd8d317008dac2a85fe8595fcdd4b57
Prerna Jaiswal <prerna11081@iiitd.ac.in>
2014-06-06 14:26:06 +0530

    edit routs.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Edu::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root to: 'welcome#index'
   #root :to => "home#index"
 
   # Example of regular route:
